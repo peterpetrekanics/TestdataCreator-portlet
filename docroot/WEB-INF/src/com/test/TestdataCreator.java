9ce8e81b26b0651d225fe8005a5467872aa1fa9c
peterpetrekanics <peter.petrekanics@liferay.com>
2016-08-17 10:29:22 +0200

    initial commit

---
+++ docroot/WEB-INF/src/com/test/TestdataCreator.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class TestdataCreator
+ */
+public class TestdataCreator extends MVCPortlet {
+ 
+
+}
