3d3ac0d4318e2e22a354c447e1285b599317105c
peterpetrekanics <peter.petrekanics@liferay.com>
2016-08-19 16:10:40 +0200

    basic portlet structure added

--- docroot/WEB-INF/src/com/test/TestdataCreator.java
+++ docroot/WEB-INF/src/com/test/TestdataCreator.java
@@ -1,11 +1,102 @@
 package com.test;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.User;
+import com.liferay.portal.model.UserGroup;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.UserGroupLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.ProcessAction;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
 /**
  * Portlet implementation class TestdataCreator
  */
 public class TestdataCreator extends MVCPortlet {
- 
+	static long companyId;
+	static User adminUser;
+
+	public void processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws IOException,
+			PortletException {
+		System.out.println("processAction starts..");
+
+		System.out.println("processAction ends..");
+	}
+
+	@Override
+	public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse) throws IOException,
+			PortletException {
+		// TODO Auto-generated method stub
+		// super.serveResource(resourceRequest, resourceResponse);
+		System.out.println("serveResource starts..");
+
+		// Getting the value for companyId
+		Company company = null;
+		try {
+			company = CompanyLocalServiceUtil.getCompanies().iterator().next();
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		companyId = company.getCompanyId();
+
+		// Getting the id of the admin user
+		try {
+			adminUser = UserLocalServiceUtil.getUserByEmailAddress(companyId, "test@liferay.com");
+		} catch (PortalException e) {
+			e.printStackTrace();
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		// System.out.println(adminUser.getUserId());
+
+		String performAction = ParamUtil.get(resourceRequest, "portletAction", "");
+
+		switch (performAction) {
+		case "createUsers":
+			;
+			break;
+			
+		case "deleteUsers":
+			deleteUsers();
+			break;
+
+		default:
+			;
+			break;
+		}
+		System.out.println("The following button was pressed: " + performAction);
+
+		
+		
+		
+		String myUserGroupName = "testUserGroup";
+		long userGroupId = 0;
+		try {
+			UserGroup myUserGroup = UserGroupLocalServiceUtil.getUserGroup(companyId, myUserGroupName);
+			userGroupId = myUserGroup.getUserGroupId();
+		} catch (PortalException e) {
+		} catch (SystemException e) {
+		}
+		// System.out.println(userGroupId);
+
+		System.out.println("serveResource ends..");
+	}
+
+	private void deleteUsers() {
+		
+	}
 
 }
--- docroot/html/testdatacreator/view.jsp
+++ docroot/html/testdatacreator/view.jsp
@@ -1,5 +1,51 @@
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet"%>
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %>
+<%@ taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %>
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+<%@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
+<%@page import="com.liferay.portal.kernel.util.Constants" %>
+<%@page import="com.liferay.portal.kernel.util.GetterUtil" %>
+<%@page import="com.liferay.portal.kernel.util.StringPool" %>
+<%@page import="com.liferay.portal.kernel.portlet.LiferayWindowState"%>
+<%@page import="com.liferay.portal.theme.ThemeDisplay"%>
+<%@page import="com.liferay.portal.theme.PortletDisplay"%>
+<%@page import="com.liferay.portal.util.PortalUtil"%>
+<%@page import="javax.portlet.RenderRequest"%>
+<%@page import="javax.portlet.ActionRequest"%>
+<%@page import="javax.portlet.PortletURL" %>
 
 <portlet:defineObjects />
 
-This is the <b>Testdata Creator</b> portlet in View mode.
+<portlet:resourceURL var="resourceUrl1">
+	<portlet:param name="portletAction" value="createUsers"/> 
+	</portlet:resourceURL>
+	
+	<a href="#" onclick="callServeResource()">Create specified nr of users with specified name</a>
+	<script type="text/javascript">
+	function callServeResource(){
+		AUI().use('aui-io-request', function(A){
+			A.io.request('${resourceUrl1}');	 
+		});
+	}
+	</script>
+	
+	
+	<br>-------------------------------<br>
+	
+	<portlet:resourceURL var="resourceUrl2">
+	<portlet:param name="portletAction" value="deleteUsers"/> 
+	</portlet:resourceURL>
+	
+	<a href="#" onclick="callServeResource2()">Delete all non-admin users</a>
+	<script type="text/javascript">
+	function callServeResource2(){
+		AUI().use('aui-io-request', function(A){
+			A.io.request('${resourceUrl2}');	 
+		});
+	}
+	</script>
+	
+	<br>-------------------------------<br>
+	
