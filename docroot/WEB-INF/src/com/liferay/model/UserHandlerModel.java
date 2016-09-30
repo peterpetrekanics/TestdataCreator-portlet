e4f8cb1dd36de9ed1730ced9e2b0ddd5158856d6
peterpetrekanics <peter.petrekanics@liferay.com>
2016-09-30 14:56:02 +0200

    Parameters for GroupLocalServiceUtil.addGroup added to the model Site creation works, tests successful.

--- docroot/WEB-INF/src/com/liferay/model/UserHandlerModel.java
+++ docroot/WEB-INF/src/com/liferay/model/UserHandlerModel.java
@@ -2,9 +2,14 @@ package com.liferay.model;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.UserGroup;
 import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.UserGroupLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
@@ -198,20 +203,30 @@ public class UserHandlerModel {
 	}
 	
 	public static int getSiteCount() throws SystemException {
-		return GroupLocalServiceUtil.getGroupsCount();
+		//Use -19 because there are 19 technical sites that we don't want to show in the count.
+		return GroupLocalServiceUtil.getGroupsCount()-19;
 	}
 
 	
 	public void createSite(long companyId, long adminUserId,
 			String newSiteName, int newSiteCount) {
 
-		ServiceContext serviceContext = null;
+		ServiceContext serviceContext = new ServiceContext();
 		
 		for (int currentSiteNumber = 1; currentSiteNumber <= newSiteCount; currentSiteNumber++) {
-			//try {
-			//	GroupLocalServiceUtil.addGroup(adminUserId, companyId,
-			//			newUserGroupName + String.format("%06d", currentUserGroupNumber),
-			//			"description", serviceContext);
+			try {
+				
+				//Add the group
+				Group group = GroupLocalServiceUtil.addGroup(adminUserId, GroupConstants.DEFAULT_PARENT_GROUP_ID, 
+						Group.class.getName(), 0, GroupConstants.DEFAULT_LIVE_GROUP_ID, newSiteName + currentSiteNumber, "", 
+						GroupConstants.TYPE_SITE_OPEN, true, GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION, 
+						"/" + newSiteName + currentSiteNumber, true, true, serviceContext);
+				
+				//Add the page
+				Layout myPage =  LayoutLocalServiceUtil.addLayout(adminUserId, group.getGroupId(), false, 
+				                                        0, "Page1", "Page1", "Page1", 
+				                                        LayoutConstants.TYPE_PORTLET, false, "/page1", serviceContext);
+				
 			} catch (PortalException e) {
 				e.printStackTrace();
 			} catch (SystemException e) {
